7831bc27ab18932a31971e0c5f09d7eb799f8b4b
RHShoumik <rhshoumik12@gmail.com>
2022-11-21 15:05:07 +0600

    HowitWorks-Done Without Slider

--- src/pages/howItWorks/index.js
+++ src/pages/howItWorks/index.js
@@ -7,8 +7,10 @@ import { brands } from "./const/brandList";
 import HowItWorksHeroImage from "svgIcons/HowItWorksHeroImage";
 import howItWorksStepsImage1 from "../../assets/HowItWorks/HowItWorksStepsImage1.png";
 import howItWorksStepsImage2 from "../../assets/HowItWorks/HowItWorksStepsImage2.png";
+import testimonialImage from "../../assets/HowItWorks/testimonialImage.png";
 
 import PrimaryButton from "components/Buttons/PrimaryButton/PrimaryButton";
+import QuotesIcon from "svgIcons/QuotesIcon";
 import "./HowItWorks.scss";
 
 const HowItWorks = () => {
@@ -88,7 +90,6 @@ const HowItWorks = () => {
 					<div className="loveByBrands--brandArea">
 						{
 							brands.map((brand, index) => {
-
 								return (
 									<div key={index} className="loveByBrands--brandArea--logo">
 										{brand.logo}
@@ -99,6 +100,26 @@ const HowItWorks = () => {
 					</div>
 					<PrimaryButton />
 				</Container>
+			</div>
+
+			<div className="testimonials">
+				<div className="testimonials--image">
+					<img src={testimonialImage} alt="Testomonial" />
+				</div>
+				<div className="testimonials--contentArea">
+					<div className="testimonials--contentArea--inside">
+						<QuotesIcon />
+						<p className="testimonials--contentArea--inside--pragraph">At Pathao we always follow data-driven approach for everything. HypeScout‘s technology ensures that practice
+							in every step of the campaign, from planning to maximizing
+							the ROI.
+						</p>
+						<div className="testimonials--contentArea--inside--details">
+							<p className="testimonials--contentArea--inside--details--name">Sayeda Nabila Mahabub</p>
+							<p className="testimonials--contentArea--inside--details--designation">Director of Marketing & PR, Pathao</p>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</Fragment>
 	)
